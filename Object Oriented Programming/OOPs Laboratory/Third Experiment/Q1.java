4b6574bba187510766aa80b929d0ec1b105ee904
KarthikeyanSivarasu12 <128465791+KarthikeyanSivarasu12@users.noreply.github.com>
2023-08-25 10:52:24 +0530

    Added Code

---
+++ Object Oriented Programming/OOPs Laboratory/Third Experiment/Q1.java
@@ -0,0 +1,35 @@
+// Find greatest of Two numbers [ if,else]
+package
+
+import java.util.Scanner;
+
+public class Q1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the First Number:");
+        System.out.println("Enter the Second Number:");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+        if(x>y)
+        {
+            System.out.println("The greatest number is:"+" "+x);
+
+        }
+        else 
+        {
+             if(x<y)
+             {
+                System.out.println("The greatest number is :"+" "+y);
+             } else
+             {
+                System.out.println("Two numbers entered by you are equal");
+             }
+        }
+
+
+
+    }
+}
