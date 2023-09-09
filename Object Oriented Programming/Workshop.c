0ce2815432956b51f2d622b77adcf8bad1157442
KarthikeyanSivarasu12 <128465791+KarthikeyanSivarasu12@users.noreply.github.com>
2023-09-09 12:35:53 +0530

    Added Code

---
+++ Object Oriented Programming/Workshop.c
@@ -0,0 +1,9 @@
+//
+//IP Address and DNS
+//dns identifies domain name and ip address
+//ip address is a unique number assigned to a computer
+//dns is a server that translates domain names to ip addresses
+//Document object model
+
+
+//DOM Manipulation
