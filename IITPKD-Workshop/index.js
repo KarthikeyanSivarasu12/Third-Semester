471499bb458f0abd93b6e35f755171a6fb4d7eb9
KarthikeyanSivarasu12 <128465791+KarthikeyanSivarasu12@users.noreply.github.com>
2023-09-10 12:29:46 +0530

    Added Code

--- IITPKD-Workshop/index.js
+++ IITPKD-Workshop/index.js
@@ -1,1 +1,47 @@
-console.log("Hello World!");
+const express=require(express);//whatever function is 
+//there in the express module, we are importing it to variable express
+
+//Construct is used to initialize the object
+//it is a special function with same name as variable name
+//it is called when we create an object
+//we are creating an object of express
+//we are calling the constructor of express
+
+
+const app=express();//app is an object of express
+//app is a server
+
+const port=3000;//port is a variable
+//port is a number
+
+
+function func()
+{
+    console.log('server is lostening at port 3000');
+
+}
+var student=[10];
+
+
+
+
+app.listen(port,func);//listen is a function.
+
+const express=require('express')
+const app=express();
+const port=3000;
+
+app.get('/local',(req,res)=>{
+    res.send('Hello World!');
+}
+);
+app.get('/about',(req,res)=>{
+    res.send('About us');
+}
+);
+
+app.listen(port,()=>{
+    console.log('server is listening at port 3000');
+}
+);
+
