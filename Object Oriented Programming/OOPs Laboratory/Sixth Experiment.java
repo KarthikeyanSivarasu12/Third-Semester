a6692ac6a0a32a5288059e7b5304ca5694e460ee
KarthikeyanSivarasu12 <128465791+KarthikeyanSivarasu12@users.noreply.github.com>
2023-09-15 12:10:32 +0530

    Added Code

---
+++ Object Oriented Programming/OOPs Laboratory/Sixth Experiment.java
@@ -0,0 +1,27 @@
+//  Create classes that capture bank customers and bank accounts.
+//  A customer has a first and last name.
+//  An account has a customer and balance.
+//  Make objects for two accounts held by the same customer.
+
+class bcus
+{
+    String fname;
+    String lname;
+}
+
+class bacc extends bcus
+{
+    double balance;
+    
+}
+
+class baccDemo
+{
+    public static void main(String[] args)
+    {
+        bacc acc1=new bacc();
+        bacc acc2=new bacc();
+        acc1
+
+    }
+}
