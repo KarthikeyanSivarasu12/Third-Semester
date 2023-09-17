7e7f773f7b0200929022968eab231e06b7a8da3b
KarthikeyanSivarasu12 <128465791+KarthikeyanSivarasu12@users.noreply.github.com>
2023-09-18 00:11:32 +0530

    Added code related to LInked List

--- Data Structures/LL/Two.c
+++ Data Structures/LL/Two.c
@@ -783,8 +783,214 @@
 // }
 
 
-//DELETE N NODES BEFORE NODE X
+// //DELETE N NODES BEFORE NODE X
 
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// //CREATE A NODE 
+
+
+// struct node
+// {
+//     int data;
+//     struct node *next;
+
+// };
+
+// //CREATING A FUNCION CALLED GETNODE
+
+// struct node *getnode(int data)
+// {
+//     struct node *p;
+//     p=(struct node*)malloc(sizeof(struct node));
+//     p->data=data;
+//     p->next=NULL;
+//     return p;
+
+// }
+
+// //DISPLAY FUNCTION
+// void display(struct node *start)
+// {
+//     struct node *p;
+//     p=start;
+//     while(p!=NULL)
+//     {
+//         printf("%d\n",p->data);
+//         p=p->next;
+//     }
+
+
+// }
+
+// struct node *create_LL(int n)
+// {
+//     struct node *new, *start = NULL, *temp = NULL;
+//     int value;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Enter the data part of the node:");
+//         scanf("%d", &value);
+//         new = getnode(value);
+        
+//         if (i == 0)
+//         {
+//             start = new;
+//             temp = start;
+//         }
+//         else
+//         {
+//             temp->next = new;
+//             temp = new;
+//         }
+//     }
+//     return start;
+// }
+
+// struct node *getnode_atx(struct node *start,int x)
+// {
+//     struct node *currentnode;
+//     currentnode=start;
+//     for(int i=1;i<x;i++)
+//     {
+//         currentnode=currentnode->next;
+//     }
+//     return currentnode;
+
+// }
+
+// struct node *delete_nbeforex(struct node *start,int n,int x)
+// {
+//     struct node *p,*new,*start1;
+//     start1=start;
+//     p=getnode_atx(start,x);
+//     int z=(x-(n+1));
+//     if (z < 1) {
+//         // If z is less than 1, we can't delete nodes before the start of the list
+//         return start1;
+//     }
+
+//     new=getnode_atx(start,z);
+//     new->next=p;
+//     return start1;
+
+// }
+
+
+
+// int main(void)
+// {
+//   int n;
+//   struct node *start;
+
+//   printf("Enter the no of nodes to be created:");
+//   scanf("%d",&n);
+//   start=create_LL(n);
+
+//   int a,x;
+//   printf("Enetr the number of nodes to be deleted :");
+//   scanf("%d",&a);
+//   printf("Enter the node number before which the  nodes have to be deleted:");
+//   scanf("%d",&x);
+  
+//   start=delete_nbeforex(start,a,x);
+//   display(start);
+//   return 0;
+
+
+// }
+
+
+
+
+// //DISPLAY THE LINKED LIST IN AN ORDERLY FASHION ALONG WITH THE ADDRESS OF EACH NODE 
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// //CREATE A NODE 
+
+
+// struct node
+// {
+//     int data;
+//     struct node *next;
+
+// };
+
+// //CREATING A FUNCION CALLED GETNODE
+
+// struct node *getnode(int data)
+// {
+//     struct node *p;
+//     p=(struct node*)malloc(sizeof(struct node));
+//     p->data=data;
+//     p->next=NULL;
+//     return p;
+
+// }
+
+// //DISPLAY FUNCTION
+// void display(struct node *start) {
+//     struct node *p = start;
+//     int i = 1;
+//     while (p != NULL) {
+//         printf("The data part of node %d is %d\n", i, p->data);
+//         if (p->next != NULL) {
+//             printf("The address of node %d is %p\n", i, (void *)p);
+//         } else {
+//             printf("The address of node %d is NULL\n", i);
+//         }
+//         printf("\n\n");
+//         p = p->next;
+//         i++;
+//     }
+// }
+
+
+// struct node *create_LL(int n)
+// {
+//     struct node *new, *start = NULL, *temp = NULL;
+//     int value;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Enter the data part of the node:");
+//         scanf("%d", &value);
+//         new = getnode(value);
+        
+//         if (i == 0)
+//         {
+//             start = new;
+//             temp = start;
+//         }
+//         else
+//         {
+//             temp->next = new;
+//             temp = new;
+//         }
+//     }
+
+//     return start;
+// }
+
+// int main(void)
+// {
+
+//   int n;
+//   struct node *start;
+
+//   printf("Enter the no of nodes to be created:");
+//   scanf("%d",&n);
+//   start=create_LL(n);
+//   display(start);
+//   return 0;
+
+// }
+
+//SEARCH FOR NODE X
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -861,43 +1067,26 @@ struct node *getnode_atx(struct node *start,int x)
 
 }
 
-struct node *delete_nbeforex(struct node *start,int n,int x)
-{
-    struct node *p,*new,*start1;
-    start1=start;
-    p=getnode_atx(start,x);
-    int z=(x-(n+1));
-    if (z < 1) {
-        // If z is less than 1, we can't delete nodes before the start of the list
-        return start1;
-    }
-
-    new=getnode_atx(start,z);
-    new->next=p;
-    return start1;
-
-}
-
-
-
 int main(void)
 {
   int n;
   struct node *start;
+  struct node *currentnode;
 
   printf("Enter the no of nodes to be created:");
   scanf("%d",&n);
   start=create_LL(n);
-
-  int a,x;
-  printf("Enetr the number of nodes to be deleted :");
-  scanf("%d",&a);
-  printf("Enter the node number before which the  nodes have to be deleted:");
+  int x;
+  printf("Enter the node number to search :");
   scanf("%d",&x);
-  
-  start=delete_nbeforex(start,a,x);
-  display(start);
+
+  currentnode=getnode_atx(start,x);
+  printf("The data part of node %d is %d",x,currentnode->data);
+  printf("The address part of node %d is %p",x,currentnode);
   return 0;
-
-
+  
 }
+
+
+
+
